baf3c622a457364ff09562706abe5b6e9011dd9e
Ayz Pat <sayz@ADMINs-MacBook-Pro.local>
2016-10-06 16:50:40 +0530

    First commit

---
+++ app/hero.ts
@@ -0,0 +1,14 @@
+export class Model {
+
+  constructor(
+   
+    public name: string,
+    public email: string,
+    public company: string,
+    public designation:string,
+    public location:string
+
+  ) {  }
+
+}
+
